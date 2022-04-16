35dabfe3f3db537371f5a690304dfe3282514a5b
iofuwisa <ootuka70@gmail.com>
2022-04-16 21:27:29 +0900

    Create swift env

---
+++ README.md
@@ -0,0 +1,17 @@
+## How to exec
+
+### Execute
+
+1. Execute play_swift.swift
+
+```
+docker-compose run main
+```
+
+### Execute Test
+
+1. Execute Test
+
+```
+ docker-compose run test
+```
---
+++ Tests/play_swiftTests/play_swiftTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import play_swift
+
+final class play_swiftTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(play_swift().text, "Hello, World!")
+    }
+}
